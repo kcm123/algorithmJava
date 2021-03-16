28f1cdde4764c3f7266fe2fae6523db4d85282eb
name <chanmi5857@gmail.com>
2021-03-16 16:13:13 +0900

    Q4 Answer

--- src/a_hash/Q4_BestAlbum.java
+++ src/a_hash/Q4_BestAlbum.java
@@ -1,8 +1,6 @@
 package a_hash;
 
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
 
 //    스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
 //    속한 노래가 많이 재생된 장르를 먼저 수록합니다.
@@ -36,22 +34,86 @@ public class Q4_BestAlbum {
         solution(genres, plays);
     }
     public static int[] solution(String[] genres, int[] plays) {
-        int[] answer = {};
-        Map<String, Object> map = new HashMap<>();
-        for(int i=0; i < genres.length; i++){
-            if(!map.containsKey(genres[i])){
-                map.put(genres[i], plays[i]);
-            }else{
-                int val = (int)map.get(genres[i]) + plays[i];
-                map.put(genres[i], val);
-            }
-        }
-        int max = 0;
-        map.keySet().forEach(k -> {
-            if(max < (int)map.get(k)){
+        int[] answer;
 
+        List<Integer> answerlist = new ArrayList<Integer>();
+        HashMap<String, Integer> genrePlayHashMap = new HashMap<String, Integer>();
+
+        for(int i =0; i<genres.length; i++) {
+            genrePlayHashMap.put(genres[i], genrePlayHashMap.getOrDefault(genres[i], 0)+plays[i]);
+        }
+        Set<String> key = genrePlayHashMap.keySet();
+        //플레이수 : 장르 를 담을 해쉬맵
+        HashMap<Integer, String> playGenreHashMap = new HashMap<Integer, String>();
+        for(String k : key) {
+            playGenreHashMap.put(genrePlayHashMap.get(k), k);
+        }
+
+        //플레이수 : 장르  오름차순
+        TreeMap<Integer, String> treeMap = new TreeMap<Integer, String>(playGenreHashMap);
+
+        int treeMapSize = treeMap.size();
+        for(int i =0; i<treeMapSize; i++) {
+            String genre = treeMap.lastEntry().getValue();
+            int deleteKey = treeMap.lastEntry().getKey(); // TreeMap 지울때 사용.
+            int max = -1, nextmax = -1;
+            int maxIndex = -1, nextmaxIndex = -1;
+
+            //해당 장르에서 가장 큰놈. 둘째놈 list에 넣기
+            for(int g =0; g<genres.length; g++) {
+                if(genres[g].equals(genre)) {
+                    //check 는 1,2 번째 수가 같은 경우.
+                    // 같지 않아야 true
+                    boolean check = max != plays[g];
+                    max = Math.max(max, plays[g]);
+                    if(plays[g] == max && check) {
+                        maxIndex = g;
+                    }
+                }
             }
-        });
+            plays[maxIndex] = -1;
+            genres[maxIndex] = "-1";
+            answerlist.add(maxIndex);
+
+            for(int g =0; g<genres.length; g++) {
+                if(genres[g].equals(genre)) {
+                    nextmax = Math.max(nextmax, plays[g]);
+                    if(plays[g] == nextmax) {
+                        nextmaxIndex = g;
+                    }
+                }
+            }
+            // 곡이 하나인 경우 -1 로 계속남게됨
+            if(nextmaxIndex != -1) {
+                plays[nextmaxIndex] = -1;
+                genres[nextmaxIndex] = "-1";
+                answerlist.add(nextmaxIndex);
+            }
+            //삭제해주어야 lastEntry 할때 차수의 장르가 나오게 됨.
+            treeMap.remove(deleteKey);
+        }
+        answer = new int[answerlist.size()];
+        for(int i = 0; i<answerlist.size(); i++) {
+            answer[i]= answerlist.get(i);
+        }
+
         return answer;
+//        int[] answer = {};
+//        Map<String, Object> map = new HashMap<>();
+//        for(int i=0; i < genres.length; i++){
+//            if(!map.containsKey(genres[i])){
+//                map.put(genres[i], plays[i]);
+//            }else{
+//                int val = (int)map.get(genres[i]) + plays[i];
+//                map.put(genres[i], val);
+//            }
+//        }
+//        int max = 0;
+//        map.keySet().forEach(k -> {
+//            if(max < (int)map.get(k)){
+//
+//            }
+//        });
+//        return answer;
     }
 }
