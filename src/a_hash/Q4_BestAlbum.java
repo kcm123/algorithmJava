c5a0b1c249bb4d562d50586e7cc809ea189f5b93
name <chanmi5857@gmail.com>
2021-03-16 16:05:45 +0900

    Q4 Fail

--- src/a_hash/Q4_BestAlbum.java
+++ src/a_hash/Q4_BestAlbum.java
@@ -1,9 +1,15 @@
 package a_hash;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
+
 //    스트리밍 사이트에서 장르 별로 가장 많이 재생된 노래를 두 개씩 모아 베스트 앨범을 출시하려 합니다. 노래는 고유 번호로 구분하며, 노래를 수록하는 기준은 다음과 같습니다.
 //    속한 노래가 많이 재생된 장르를 먼저 수록합니다.
 //    장르 내에서 많이 재생된 노래를 먼저 수록합니다.
 //    장르 내에서 재생 횟수가 같은 노래 중에서는 고유 번호가 낮은 노래를 먼저 수록합니다.
-//    노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때, 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
+//    노래의 장르를 나타내는 문자열 배열 genres와 노래별 재생 횟수를 나타내는 정수 배열 plays가 주어질 때
+//    , 베스트 앨범에 들어갈 노래의 고유 번호를 순서대로 return 하도록 solution 함수를 완성하세요.
 //    제한사항
 //    genres[i]는 고유번호가 i인 노래의 장르입니다.
 //    plays[i]는 고유번호가 i인 노래가 재생된 횟수입니다.
@@ -24,8 +30,28 @@ package a_hash;
 //    고유 번호 1: 600회 재생
 //    따라서 pop 장르의 [4, 1]번 노래를 먼저, classic 장르의 [3, 0]번 노래를 그다음에 수록합니다.
 public class Q4_BestAlbum {
-    public int[] solution(String[] genres, int[] plays) {
+    public static void main(String[] args){
+        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
+        int[] plays = {500, 600, 150, 800, 2500};
+        solution(genres, plays);
+    }
+    public static int[] solution(String[] genres, int[] plays) {
         int[] answer = {};
+        Map<String, Object> map = new HashMap<>();
+        for(int i=0; i < genres.length; i++){
+            if(!map.containsKey(genres[i])){
+                map.put(genres[i], plays[i]);
+            }else{
+                int val = (int)map.get(genres[i]) + plays[i];
+                map.put(genres[i], val);
+            }
+        }
+        int max = 0;
+        map.keySet().forEach(k -> {
+            if(max < (int)map.get(k)){
+
+            }
+        });
         return answer;
     }
 }
