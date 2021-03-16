82ce2d2237803f8f9efcdcbbf0b8969d50856abe
name <chanmi5857@gmail.com>
2021-03-16 13:36:26 +0900

    Q1, Q2

--- src/a_hash/Q1_Athlete.java
+++ src/a_hash/Q1_Athlete.java
@@ -1,4 +1,7 @@
 package a_hash;
+
+import java.util.*;
+
 //    수많은 마라톤 선수들이 마라톤에 참여하였습니다. 단 한 명의 선수를 제외하고는 모든 선수가 마라톤을 완주하였습니다.
 //    마라톤에 참여한 선수들의 이름이 담긴 배열 participant와 완주한 선수들의 이름이 담긴 배열 completion이 주어질 때, 완주하지 못한 선수의 이름을 return 하도록 solution 함수를 작성해주세요.
 //    제한사항
@@ -19,10 +22,36 @@ package a_hash;
 //    예제 #3
 //    "mislav"는 참여자 명단에는 두 명이 있지만, 완주자 명단에는 한 명밖에 없기 때문에 한명은 완주하지 못했습니다.
 public class Q1_Athlete {
-
-    public String solution(String[] participant, String[] completion) {
+    public static void main(String[] arg){
+        String[] participant = {"leo", "kiki", "eden"};
+        String[] completion = {"eden", "kiki"};
+        System.out.println(solution(participant, completion));
+    }
+    public static String solution(String[] participant, String[] completion) {
+        // code
         String answer = "";
+        HashMap<String, Integer> hm = new HashMap<>();
+        for (String player : participant) hm.put(player, hm.getOrDefault(player, 0) + 1);
+        for (String player : completion) hm.put(player, hm.get(player) - 1);
 
+        for (String key : hm.keySet()) {
+            if (hm.get(key) != 0){
+                answer = key;
+            }
+        }
         return answer;
+        // myCode
+//        String answer = "";
+//        Arrays.sort(participant);
+//        Arrays.sort(completion);
+//        for(int i = 0; i < participant.length; i++){
+//            if(i >= completion.length){
+//                return participant[i];
+//            }
+//            if(!participant[i].equals(completion[i])){
+//                return participant[i];
+//            }
+//        }
+//        return answer;
     }
 }
--- src/a_hash/Q2_PhoneNo.java
+++ src/a_hash/Q2_PhoneNo.java
@@ -1,4 +1,7 @@
 package a_hash;
+
+import java.util.Arrays;
+
 //    전화번호부에 적힌 전화번호 중, 한 번호가 다른 번호의 접두어인 경우가 있는지 확인하려 합니다.
 //    전화번호가 다음과 같을 경우, 구조대 전화번호는 영석이의 전화번호의 접두사입니다.
 //    구조대 : 119
@@ -22,8 +25,41 @@ package a_hash;
 //    입출력 예 #3
 //    첫 번째 전화번호, “12”가 두 번째 전화번호 “123”의 접두사입니다. 따라서 답은 false입니다.
 public class Q2_PhoneNo {
-    public boolean solution(String[] phone_book) {
-        boolean answer = true;
-        return answer;
+    public static void main(String[] arg){
+        String[] phone_book = {"12","123","1235","567","88"};
+        System.out.println(solution(phone_book));
+    }
+    public static boolean solution(String[] phone_book) {
+        // 1. contains
+        Arrays.sort(phone_book);
+        boolean result = true;
+        for (int i=0; i<phone_book.length-1; i++) {
+            if (phone_book[i+1].contains(phone_book[i])) {
+                result = false;
+                break;
+            }
+        }
+        return result;
+        // 2. stream 사용
+//        String text = String.format(" %s", String.join(" ", phone_book));
+//        return !Arrays.stream(phone_book)
+//                .anyMatch(s -> text.split(String.format(" %s", s)).length > 2);
+
+        // myCode
+//        boolean answer = true;
+//        Arrays.sort(phone_book);
+//        for(int i = 0; i < phone_book.length; i++){
+//            for(int j = 0; j < phone_book.length; j++){
+//                if(i ==j) continue;
+//                if(phone_book[j].indexOf(phone_book[i]) == 0) return false;
+//            }
+//        }
+//        for(int i=0; i<phone_book.length-1; i++) {
+//            for(int j=i+1; j<phone_book.length; j++) {
+//                if(phone_book[i].startsWith(phone_book[j])) {return false;}
+//                if(phone_book[j].startsWith(phone_book[i])) {return false;}
+//            }
+//        }
+//        return answer;
     }
 }
