eba22d81311f19c3f2b20bb5b8ad9dbdaab1f9ae
name <chanmi5857@gmail.com>
2021-03-31 17:27:40 +0900

    스택/큐 1,2,3

--- src/b_stackQueue/Q1_Truck.java
+++ src/b_stackQueue/Q1_Truck.java
@@ -1,35 +1,93 @@
 package b_stackQueue;
-//    트럭 여러 대가 강을 가로지르는 일 차선 다리를 정해진 순으로 건너려 합니다. 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 알아내야 합니다. 트럭은 1초에 1만큼 움직이며, 다리 길이는 bridge_length이고 다리는 무게 weight까지 견딥니다.
+
+import java.util.*;
+
+//    트럭 여러 대가 강을 가로지르는 일 차선 다리를 정해진 순으로 건너려 합니다.
+//    모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 알아내야 합니다.
+//    트럭은 1초에 1만큼 움직이며, 다리 길이는 bridge_length이고 다리는 무게 weight까지 견딥니다.
 //    ※ 트럭이 다리에 완전히 오르지 않은 경우, 이 트럭의 무게는 고려하지 않습니다.
 //
 //    예를 들어, 길이가 2이고 10kg 무게를 견디는 다리가 있습니다. 무게가 [7, 4, 5, 6]kg인 트럭이 순서대로 최단 시간 안에 다리를 건너려면 다음과 같이 건너야 합니다.
 //
-//    경과 시간	다리를 지난 트럭	다리를 건너는 트럭	대기 트럭
-//    0	[]	[]	[7,4,5,6]
-//    1~2	[]	[7]	[4,5,6]
-//    3	[7]	[4]	[5,6]
-//    4	[7]	[4,5]	[6]
-//    5	[7,4]	[5]	[6]
-//    6~7	[7,4,5]	[6]	[]
-//    8	[7,4,5,6]	[]	[]
+//    경과 시간	다리를 지난 트럭	다리를 건너는 트럭	    대기 트럭
+//    0	        []	            []	                [7,4,5,6]
+//    1~2	    []	            [7]	                [4,5,6]
+//    3	        [7]	            [4]	                [5,6]
+//    4	        [7]	            [4,5]	            [6]
+//    5	        [7,4]	        [5]	                [6]
+//    6~7	    [7,4,5]	        [6]	                []
+//    8	        [7,4,5,6]	    []	                []
 //    따라서, 모든 트럭이 다리를 지나려면 최소 8초가 걸립니다.
-//
 //    solution 함수의 매개변수로 다리 길이 bridge_length, 다리가 견딜 수 있는 무게 weight, 트럭별 무게 truck_weights가 주어집니다. 이때 모든 트럭이 다리를 건너려면 최소 몇 초가 걸리는지 return 하도록 solution 함수를 완성하세요.
-//
 //    제한 조건
-//    bridge_length는 1 이상 10,000 이하입니다.
-//    weight는 1 이상 10,000 이하입니다.
-//    truck_weights의 길이는 1 이상 10,000 이하입니다.
-//    모든 트럭의 무게는 1 이상 weight 이하입니다.
+//    bridge_length는 1 이상 10,000 이하입니다.weight는 1 이상 10,000 이하입니다.truck_weights의 길이는 1 이상 10,000 이하입니다.모든 트럭의 무게는 1 이상 weight 이하입니다.
 //    입출력 예
 //    bridge_length	weight	truck_weights	return
 //    2	10	[7,4,5,6]	8
 //    100	100	[10]	101
 //    100	100	[10,10,10,10,10,10,10,10,10,10]	110
 public class Q1_Truck { // Level2_다리를 지나는 트럭
-
-    public int solution(int bridge_length, int weight, int[] truck_weights) {
+    public static void main(String[] arg){
+        int[] truck_weight = {100};
+        System.out.println(solution(100, 100, truck_weight));
+    }
+// |	7	|
+// |	4	|
+// |	5	|
+// |	6	|
+// ㅡㅡㅡㅡㅡㅡ
+    public static int solution(int bridge_length, int weight, int[] truck_weights) {
+        Stack<Integer> truckStack = new Stack<>();
+        Map<Integer, Integer> bridgeMap = new HashMap<>();
+        for (int i = truck_weights.length-1; i >= 0; i--){
+            truckStack.push(truck_weights[i]);
+        }
         int answer = 0;
+        int sum = 0;
+        while(true) {
+            answer++;
+            if (bridgeMap.containsKey(answer)){
+                bridgeMap.remove(answer);
+            }
+            sum = bridgeMap.values().stream().mapToInt(Number::intValue).sum();
+            if (!truckStack.isEmpty()) { // 트럭이 비어있지 않을떄
+                if (sum + truckStack.peek() <= weight){ //
+                    bridgeMap.put(answer + bridge_length, truckStack.pop());
+                }
+            }
+            if (bridgeMap.isEmpty() && truckStack.isEmpty()){ // 맨마지막
+                break;
+            }
+        }
         return answer;
+        // myCode_1번만 성공_실패
+//        int answer = 0; boolean flag = true;
+//        Queue<Integer> truck = new LinkedList<>();Queue<Integer> passing = new LinkedList<>();Queue<Integer> passed = new LinkedList<>();
+//        for(int i : truck_weights){ truck.add(i); }
+//        while(flag){
+//            if(passed.size() != truck_weights.length){
+//                if(truck.size() != 0){
+//                    int sum = passing.stream().mapToInt(Integer::intValue).sum() + truck.peek();
+//                    if(sum <= weight){
+//                        passing.add(truck.peek());
+//                        truck.poll();
+//                    }else{
+//                        passing.add(truck.peek());
+//                        truck.poll();
+//                        passed.add(passing.peek());
+//                        passing.poll();
+//                        answer++;
+//                    }
+//                    answer++;
+//                }else{
+//                    passed.add(passing.peek());
+//                    passing.poll();
+//                    answer++;
+//                }
+//            }else{
+//                flag = false;
+//            }
+//        }
+//        return answer;
     }
 }
--- src/b_stackQueue/Q3_DevFunction.java
+++ src/b_stackQueue/Q3_DevFunction.java
@@ -1,4 +1,7 @@
 package b_stackQueue;
+
+import java.util.*;
+
 //    프로그래머스 팀에서는 기능 개선 작업을 수행 중입니다. 각 기능은 진도가 100%일 때 서비스에 반영할 수 있습니다.
 //
 //    또, 각 기능의 개발속도는 모두 다르기 때문에 뒤에 있는 기능이 앞에 있는 기능보다 먼저 개발될 수 있고, 이때 뒤에 있는 기능은 앞에 있는 기능이 배포될 때 함께 배포됩니다.
@@ -11,13 +14,14 @@ package b_stackQueue;
 //    작업 속도는 100 이하의 자연수입니다.
 //    배포는 하루에 한 번만 할 수 있으며, 하루의 끝에 이루어진다고 가정합니다. 예를 들어 진도율이 95%인 작업의 개발 속도가 하루에 4%라면 배포는 2일 뒤에 이루어집니다.
 //    입출력 예
-//    progresses	speeds	return
-//    [93, 30, 55]	[1, 30, 5]	[2, 1]
+//    progresses	            speeds	            return
+//    [93, 30, 55]	            [1, 30, 5]	        [2, 1]
 //    [95, 90, 99, 99, 80, 99]	[1, 1, 1, 1, 1, 1]	[1, 3, 2]
 //    입출력 예 설명
 //    입출력 예 #1
 //    첫 번째 기능은 93% 완료되어 있고 하루에 1%씩 작업이 가능하므로 7일간 작업 후 배포가 가능합니다.
-//    두 번째 기능은 30%가 완료되어 있고 하루에 30%씩 작업이 가능하므로 3일간 작업 후 배포가 가능합니다. 하지만 이전 첫 번째 기능이 아직 완성된 상태가 아니기 때문에 첫 번째 기능이 배포되는 7일째 배포됩니다.
+//    두 번째 기능은 30%가 완료되어 있고 하루에 30%씩 작업이 가능하므로 3일간 작업 후 배포가 가능합니다.
+//    하지만 이전 첫 번째 기능이 아직 완성된 상태가 아니기 때문에 첫 번째 기능이 배포되는 7일째 배포됩니다.
 //    세 번째 기능은 55%가 완료되어 있고 하루에 5%씩 작업이 가능하므로 9일간 작업 후 배포가 가능합니다.
 //
 //    따라서 7일째에 2개의 기능, 9일째에 1개의 기능이 배포됩니다.
@@ -27,9 +31,49 @@ package b_stackQueue;
 //
 //    따라서 5일째에 1개의 기능, 10일째에 3개의 기능, 20일째에 2개의 기능이 배포됩니다.
 public class Q3_DevFunction { // Level2_기능개발
-
-    public int[] solution(int[] progresses, int[] speeds) {
-        int[] answer = {};
-        return answer;
+    public static void main(String[] arg){
+        int[] progresses = {93, 30, 55};
+        int[] sppeds = {1, 30, 5};
+        solution(progresses, sppeds);
+    }
+    public static int[] solution(int[] progresses, int[] speeds) {
+        int[] answer = new int[100];
+        int day = -1;
+        for(int i=0; i<progresses.length; i++) {
+            while(progresses[i] + (day*speeds[i]) < 100) { // 93 + (-1 * 1) // 30 + (7 * 30) // 55 + (9 * 5)
+                day++;
+            }
+            answer[day]++;
+        }
+        return Arrays.stream(answer).filter(i -> i!=0).toArray();
+//        myCode_두문제 결과는 나오나 점수미달...36점_실패
+//        int no = 0;
+//        Queue<Integer> val = new LinkedList<>();
+//        Queue<Integer> rs = new LinkedList<>();
+//        List<Integer> list = new ArrayList<>();
+//        for(int i = 0; i < progresses.length; i++){
+//            int remain = 100-progresses[i];
+//            int day = remain/speeds[i] + (remain%speeds[i] == 0 ? 0 : 1);
+//            if(val.isEmpty()){
+//                val.add(day);
+//                list.add(0, 1);
+//                continue;
+//            }
+//            if(val.peek() >= day){
+//                val.add(day);
+//                int idx = list.size()-1;
+//                list.add(list.get(idx)+1);
+//                list.remove(idx);
+//            }else{
+//                no += 1;
+//                list.add(1);
+//                rs.add(no);
+//            }
+//        }
+//        int[] answer = new int[list.size()];
+//        for(int i = 0; i < list.size(); i++){
+//            answer[i] = list.get(i);
+//        }
+//        return answer;
     }
 }
