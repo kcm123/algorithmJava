ec8ba75247e436cf31d8ddb16459d2f0971e7c00
kimchanmi <chanmi5857@gmail.com>
2021-04-13 22:07:51 +0900

    네트워크

--- src/a_dfsBfs/Q2_Network.java
+++ src/a_dfsBfs/Q2_Network.java
@@ -1,5 +1,7 @@
 package a_dfsBfs;
-//    네트워크란 컴퓨터 상호 간에 정보를 교환할 수 있도록 연결된 형태를 의미합니다. 예를 들어, 컴퓨터 A와 컴퓨터 B가 직접적으로 연결되어있고, 컴퓨터 B와 컴퓨터 C가 직접적으로 연결되어 있을 때 컴퓨터 A와 컴퓨터 C도 간접적으로 연결되어 정보를 교환할 수 있습니다. 따라서 컴퓨터 A, B, C는 모두 같은 네트워크 상에 있다고 할 수 있습니다.
+//    네트워크란 컴퓨터 상호 간에 정보를 교환할 수 있도록 연결된 형태를 의미합니다. 예를 들어,
+//    컴퓨터 A와 컴퓨터 B가 직접적으로 연결되어있고, 컴퓨터 B와 컴퓨터 C가 직접적으로 연결되어 있을 때 컴퓨터 A와 컴퓨터 C도 간접적으로 연결되어 정보를 교환할 수 있습니다.
+//    따라서 컴퓨터 A, B, C는 모두 같은 네트워크 상에 있다고 할 수 있습니다.
 //    컴퓨터의 개수 n, 연결에 대한 정보가 담긴 2차원 배열 computers가 매개변수로 주어질 때, 네트워크의 개수를 return 하도록 solution 함수를 작성하시오.
 //    제한사항
 //    컴퓨터의 개수 n은 1 이상 200 이하인 자연수입니다.
@@ -17,10 +19,33 @@ package a_dfsBfs;
 //    예제 #2
 //    아래와 같이 1개의 네트워크가 있습니다.
 //    https://grepp-programmers.s3.amazonaws.com/files/ybm/7554746da2/edb61632-59f4-4799-9154-de9ca98c9e55.png
+// https://velog.io/@ajufresh/프로그래머스-네트워크-문제풀이-Java
 public class Q2_Network {
+    public static void main(String[] arg){
+        //int[][] computers = {{1, 1, 0}, {1, 1, 0}, {0, 0, 1}};
+        int[][] computers = {{1, 1, 0}, {1, 1, 1}, {0, 1, 1}};
+        System.out.println(solution(3, computers));
+    }
+    public boolean[] dfs(int[][] computers, int i, boolean[] check) {
+        check[i] = true;
 
-    public int solution(int n, int[][] computers) {
+        for (int j = 0; j < computers.length; j++) {
+            if (i != j && computers[i][j] == 1 && check[j] == false) {
+                check = dfs(computers, j, check);
+            }
+        }
+        return check;
+    }
+    public static int solution(int n, int[][] computers) {
         int answer = 0;
+        boolean[] check = new boolean[n]; // n 갯수만큼 boolean 배열을 만들고 모든 요소를 false로 초기화
+
+        for (int i = 0; i < n; i++) {
+            if (!check[i]) {
+                dfs(computers, i, check);
+                answer++;
+            }
+        }
         return answer;
     }
 }
