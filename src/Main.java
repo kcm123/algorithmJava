7f401fb0537d6bda89dcefae54e8596cf5afa686
kimchanmi <chanmi5857@gmail.com>
2021-04-22 20:06:35 +0900

    코테

--- src/Main.java
+++ src/Main.java
@@ -1,73 +1,54 @@
 import java.util.*;
 
 public class Main {
-
-    public static boolean[] visited = new boolean[9];
-    public static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();
-// 1 2 3 8 7 4 5 6
-    // BFS 함수 정의
-    public static void bfs(int start) {
-        Queue<Integer> q = new LinkedList<>();
-        q.offer(start);
-        // 현재 노드를 방문 처리
-        visited[start] = true;
-        // 큐가 빌 때까지 반복
-        while(!q.isEmpty()) {
-            // 큐에서 하나의 원소를 뽑아 출력
-            int x = q.poll();
-            System.out.print(x + " ");
-            // 해당 원소와 연결된, 아직 방문하지 않은 원소들을 큐에 삽입
-            for(int i = 0; i < graph.get(x).size(); i++) {
-                int y = graph.get(x).get(i);
-                if(!visited[y]) {
-                    q.offer(y);
-                    visited[y] = true;
-                }
-            }
+    public static void main(String[] arg){
+        int[][] needs = { { 1, 0, 0 }, {1, 1, 0}, {1, 1, 0}, {1, 0, 1}, {1, 1, 0}, {0, 1, 1} };
+//        int[] gift_cards = {4, 5, 3, 2, 1};
+//        int[] wants = {2, 4, 4, 5, 1};
+        int[] passenger = {};
+        int[][] train =  {{1,2},{1,3},{1,4},{3,5},{3,6}};
+        System.out.println(solution(6, passenger, train));
+    }
+    public static int[] solution(int n, int[] passenger, int[][] train) {
+        int[] answer = {};
+        List<List<Integer>> list = new ArrayList<>();
+        for(int i = 0; i < train.length; i++){
+            List list2 = new ArrayList<>();
+            list2.add(train[i][0]);
+            list2.add((int)train[i][1]);
+            list.add(list2);
+        }
+        return answer;
+    }
+    public void dfs(int x, boolean[] visited, List<List<Integer>> list){
+        visited[x] = true;
+        for(int i = 0; i < list.get(x).size(); i++){
+            int y = list.get(x).get(i);
+            if(!visited[y]) dfs(y, visited, list);
         }
     }
-
-    public static void main(String[] args) {
-        // 그래프 초기화
-        for (int i = 0; i < 9; i++) {
-            graph.add(new ArrayList<Integer>());
-        }
-
-        // 노드 1에 연결된 노드 정보 저장
-        graph.get(1).add(2);
-        graph.get(1).add(3);
-        graph.get(1).add(8);
-
-        // 노드 2에 연결된 노드 정보 저장
-        graph.get(2).add(1);
-        graph.get(2).add(7);
-
-        // 노드 3에 연결된 노드 정보 저장
-        graph.get(3).add(1);
-        graph.get(3).add(4);
-        graph.get(3).add(5);
-
-        // 노드 4에 연결된 노드 정보 저장
-        graph.get(4).add(3);
-        graph.get(4).add(5);
-
-        // 노드 5에 연결된 노드 정보 저장
-        graph.get(5).add(3);
-        graph.get(5).add(4);
-
-        // 노드 6에 연결된 노드 정보 저장
-        graph.get(6).add(7);
-
-        // 노드 7에 연결된 노드 정보 저장
-        graph.get(7).add(2);
-        graph.get(7).add(6);
-        graph.get(7).add(8);
-
-        // 노드 8에 연결된 노드 정보 저장
-        graph.get(8).add(1);
-        graph.get(8).add(7);
-
-        bfs(1);
-    }
-
+//    public static int solution(int[] gift_cards, int[] wants){
+//        List<Integer> want = new ArrayList<>();
+//        List<Integer> real = new ArrayList<>();
+//        for(int i : wants) want.add(i);
+//        for(int i : gift_cards) real.add(i);
+//        for(int i = 0; i < real.size(); i++){
+//            if(want.get(i) != real.get(i)){
+//                int idx = real.indexOf(want.get(i))-1;
+//                if(idx == -2) continue;
+//                int temp = real.get(i);
+//                real.remove(i);
+//                want.remove(i);
+//                idx = idx == -1 ? 0 : idx;
+//                real.remove(idx);
+//                real.add(idx, temp);
+//                i--;
+//            }else{
+//                want.remove(i);
+//                real.remove(i);
+//                i--;
+//            }
+//        }
+//        return real.size();
+//    }
 }
