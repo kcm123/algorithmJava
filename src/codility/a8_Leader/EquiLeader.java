d0ad0d65c6f113ae477ae6f9561d8976ef2a043e
name <chanmi5857@gmail.com>
2021-06-23 09:52:07 +0900

    codility 8-2

--- src/codility/a8_Leader/EquiLeader.java
+++ src/codility/a8_Leader/EquiLeader.java
@@ -28,6 +28,7 @@ package codility.a8_Leader;
 //    Write an efficient algorithm for the following assumptions:
 //    N is an integer within the range [1..100,000];
 //    each element of array A is an integer within the range [−1,000,000,000..1,000,000,000].
+// https://lipcoder.tistory.com/entry/EquiLeader-Codility
 import java.util.*;
 public class EquiLeader {
     public static void main(String[] args) {
@@ -35,46 +36,86 @@ public class EquiLeader {
         System.out.println(solution(A));
     }
     public static int solution(int[] A){
-        int rs = 0;
-        int n = A.length-3;
-        if(n < 1){
-            return rs;
-        }
-        List list = new ArrayList<>();
-        List list2 = new ArrayList<>();
-        for(int no : A){
-            list.add(no);
-        }
-        for(int i = 0; i < n; i++){
-            list2.add(A[i]);
-            list.remove(0);
-            if(A[i] == getDominator(list2) && A[i] == getDominator(list)){
-                rs++;
+        int result = 0;
+        int leader = 0, leaderCount = 0;
+        // 숫자 빈도 카운트 값 기록을 위해 해시맵 컨테이너를 선언한다.
+        HashMap<Integer, Integer> hm = new HashMap<Integer, Integer>();
+        // 카운트 값을 기록한다
+        for(int i = 0 ; i < A.length; ++i) {
+            if(hm.containsKey(A[i])) {
+                int count = hm.get(A[i]);
+                hm.replace(A[i], count + 1);
+            } else {
+                hm.put(A[i], 1);
+            }
+            // 가장 많이 나온 수 (Leader)를 기록한다.
+            if(leaderCount < hm.get(A[i])) {
+                leaderCount = hm.get(A[i]);
+                leader = A[i];
             }
         }
-        return rs;
-    }
-    public static int getDominator(List<Integer> list){
-        int rs = 0;
-        Map<Integer, Integer> map = new HashMap<>();
-        for(int i = 0; i < list.size(); i++){
-            map.put(list.get(i), map.getOrDefault(list.get(i), 0) +1);
+        // Leader 숫자를 구했으니 Leader 숫자가 각 인덱스에서 몇 개 정도 나왔는지 기록할 벡터 컨테이너를 만든다.
+        Vector<Integer> record = new Vector<Integer>();
+        int currentCount = 0;
+        // 각 인덱스위치에서 Leader의 빈도수를 기록한다.
+        for(int i = 0 ; i < A.length; ++i) {
+            if(A[i] == leader) {
+                ++currentCount;
+            }
+            record.add(currentCount);
         }
-        if(map.size() > 0){
-            List<Map.Entry<Integer, Integer>> domlist = new ArrayList<>(map.entrySet());
-            if(domlist.size() == 1) return domlist.get(0).getKey();
-            domlist.sort(Map.Entry.comparingByValue());
-            int idx = domlist.size()-1;
-            int max = domlist.get(idx).getValue();
-            if(max > list.size()/2){
-                for(int i = 0; i < list.size(); i++){
-                    if(list.get(i) == domlist.get(idx).getKey()){
-                        rs = domlist.get(idx).getKey();
-                        break;
-                    }
-                }
+        for(int i = 0; i < A.length - 1; ++i) {
+            int LeftCount = record.elementAt(i); // 둘로 쪼갰을때 왼쪽 부분 Leader 빈도수
+            int RightCount = record.lastElement() - LeftCount; // 오른쪽 부분 Leader 빈도수
+
+            int limitEquiLeft = ((i + 1) / 2) + 1;
+            int limitEquiRight = ((A.length - (i + 1)) / 2) + 1;
+            // EquiLeader 조건을 왼쪽과 오른쪽 부분 모두 만족한다면 갯수를 증가 시킨다.
+            if((LeftCount >= limitEquiLeft) && (RightCount >= limitEquiRight)) {
+                ++result;
             }
         }
-        return rs;
+        return result;
+//        int rs = 0;
+//        int n = A.length-3;
+//        if(n < 1){
+//            return rs;
+//        }
+//        List list = new ArrayList<>();
+//        List list2 = new ArrayList<>();
+//        for(int no : A){
+//            list.add(no);
+//        }
+//        for(int i = 0; i < n; i++){
+//            list2.add(A[i]);
+//            list.remove(0);
+//            if(A[i] == getDominator(list2) && A[i] == getDominator(list)){
+//                rs++;
+//            }
+//        }
+//        return rs;
     }
+//    public static int getDominator(List<Integer> list){
+//        int rs = 0;
+//        Map<Integer, Integer> map = new HashMap<>();
+//        for(int i = 0; i < list.size(); i++){
+//            map.put(list.get(i), map.getOrDefault(list.get(i), 0) +1);
+//        }
+//        if(map.size() > 0){
+//            List<Map.Entry<Integer, Integer>> domlist = new ArrayList<>(map.entrySet());
+//            if(domlist.size() == 1) return domlist.get(0).getKey();
+//            domlist.sort(Map.Entry.comparingByValue());
+//            int idx = domlist.size()-1;
+//            int max = domlist.get(idx).getValue();
+//            if(max > list.size()/2){
+//                for(int i = 0; i < list.size(); i++){
+//                    if(list.get(i) == domlist.get(idx).getKey()){
+//                        rs = domlist.get(idx).getKey();
+//                        break;
+//                    }
+//                }
+//            }
+//        }
+//        return rs;
+//    }
 }
